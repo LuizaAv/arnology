177dfd7b44339de2d6e020d49085f880ef8c628f
root <root>
2021-06-14 12:46:03 +0400

    corrected navbar' menu icon position

--- src/components/Works/workpanel.js
+++ src/components/Works/workpanel.js
@@ -1,19 +1,31 @@
 import React from "react";
-import "./workpanel.css"
+import "./workpanel.css";
+
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 export default function  WorkPanel(){
     return(
         <div className="workPanelContainer">
-            <h2 className="workpanelH2">OUR WORKS</h2>
-            <div className="workpanelLinksContainer">
-                <a style={{color: "#03989b"}} href="">All</a>
-                <a href="">Social</a>
-                <a href="">Ecommerce</a>
-                <a href="">Travel</a>
-                <a href="">Booking</a>
-                <a href="">Apps</a>
-                <a href="">Other</a>
-            </div>
+            <Grid >
+                <h2 className="headerOurWorks">OUR WORKS</h2>
+            </Grid>
+        <Grid container  spacing={3} className="mainContainerGrid">
+            <Grid item xs={12} sm={6} lg={4}>
+                <Grid>
+                    <div >
+                        <a style={{color: "#03989b"}} href="">All</a>
+                        <a href="">Social</a>
+                        <a href="">Ecommerce</a>
+                        <a href="">Travel</a>
+                        <a href="">Booking</a>
+                        <a href="">Apps</a>
+                        <a href="">Other</a>
+                    </div>
+                </Grid>
+            </Grid>
+        </Grid>
         </div>
     )
 }
--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -5,18 +5,40 @@ import "./works.css"
 import WorkPanel from "./workpanel";
 import Footer from "../footer";
 import ProjectLines from "./projectLines";
+import {useMediaQuery} from "react-responsive";
 
 
 export default function Works(){
+
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1024px)' })
+    const isDesktopOrLaptop  = useMediaQuery({ query: '(min-width: 1025px)' })
+
     return(
         <div className="worksPageContainer">
-            <Navbar/>
-            <WorkPanel/>
-            <div className="picturesGridContainer">
-                <ProjectLines/>
-                <ProjectLines/>
-            </div>
-            <Footer/>
+            {
+                isDesktopOrLaptop &&
+                    <>
+                        <Navbar />
+                        <WorkPanel />
+                        <div className="picturesGridContainer">
+                            <ProjectLines/>
+                            <ProjectLines/>
+                        </div>
+                        <Footer/> 
+                    </>
+            }
+            {
+                isTabletOrMobile &&
+                    <>
+                        <Navbar />
+                        <WorkPanel />
+                        <div className="picturesGridContainer1">
+                            <ProjectLines/>
+                            <ProjectLines/>
+                        </div>
+                        <Footer/>
+                    </>
+            }
         </div>
     )
 }
